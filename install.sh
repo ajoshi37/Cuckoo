b562d5ef5e065c1122718b34436b38f000939268
ajoshi37 <54622496+ajoshi37@users.noreply.github.com>
2021-12-17 20:26:36 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install python python-pip python-dev libffi-dev libssl-dev -y
+#sudo apt-get install python-virtualenv python-setuptools -y
+sudo apt-get install libjpeg-dev zlib1g-dev swig -y
+sudo apt-get install mongodb -y
+sudo apt-get install postgresql libpq-dev -y
